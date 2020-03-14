5aafa6f3b57525613ab7299faaea80a4c142c137
root <root@bench.mac.wales>
2020-03-14 19:34:34 -0400

    up action

--- cloud/action.py
+++ cloud/action.py
@@ -4,22 +4,7 @@ import yaml
 class Action:
 
     def __init__(self):
-        self.config = self.load_config()
+        pass
 
-    def load_config(self):
-        try:
-            with open('cloud.yaml') as f:
-                return yaml.safe_load(f)
-        except:
-            print("cannot open cloud.yaml")
-            exit(1)
-
-    def guests(self):
-      return self.config['guests'].items()
-
-    def list(self):
-        for (name, guest) in self.guests():
-            print(name, guest) 
-    def list(self):
-        for (name, guest) in self.guests():
-            print(name, guest) 
+    def up(self, name, guest):
+        print("up", name, guest)
--- cloud/command.py
+++ cloud/command.py
@@ -1,21 +1,37 @@
 
+import yaml
 from cloud.action import Action
 
 class Command:
 
     def __init__(self):
+        self.config = self.load_config()
         self.action = Action()
         self._ls = self._list
+
+    def load_config(self):
+        try:
+            with open('cloud.yaml') as f:
+                return yaml.safe_load(f)
+        except:
+            print("cannot open cloud.yaml")
+            exit(1)
+
+    def guests(self):
+      return self.config['guests'].items()
 
     def help(self, args = []):
         print("help", args)
 
     def _list(self, args):
         print("list", args)
-        self.action.list()
+        for (name, guest) in self.guests():
+            print(name, guest)
 
     def _up(self, args):
         print("up", args)
+        for (name, guest) in self.guests():
+            self.action.up(name, guest)
 
     def run(self, cmd, args):
         try:
