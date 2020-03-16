60e73dc755925e4e14e88e04342cef6d4fa1aa81
root <root@bench.mac.wales>
2020-03-16 03:34:33 -0400

    vms list guests and ip addresses

--- v.py
+++
@@ -1,9 +0,0 @@
-import libvirt
-conn = libvirt.open()
-
-for domain in conn.listAllDomains():
-    print(domain)
-    #metadata = domain.metadata(libvirt.VIR_DOMAIN_METADATA_ELEMENT, "http://spinup.io/instance")
-    print(domain.name())
-    nif = domain.interfaceAddresses(libvirt.VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_LEASE)
-    print(nif)
