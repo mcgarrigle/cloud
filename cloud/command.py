b3836f51f374bf73e1c78f8001db708a5a161a4d
root <root@bench.mac.wales>
2020-03-22 19:04:09 -0400

    list has better formatting

--- cloud/command.py
+++ cloud/command.py
@@ -1,4 +1,6 @@
 import os
+import re
+import inspect
 import yaml
 import dns.resolver
 from cloud.action import Action
@@ -26,26 +28,34 @@ class Command:
             exit(1)
 
     def _help(self, args = []):
-        print("help", args)
+        """ show help """
+        print("cloud help")
+        methods = [func for func in dir(Command) if re.match(r'^_[a-z]', func)]
+        for method in sorted(list(methods)):
+            print('*', method.lstrip('_'), '-', inspect.getdoc(eval(f"self.{method}")))
 
     def _list(self, args):
+        """ show status of all guests """
         domains = Domains().list()
         for guest in self.guests:
             domain = domains.get(guest['name'])
             if domain:
-                print(domain.name, domain.state, domain.addr)
+                print(f"{domain.name: <15} {domain.state: <10} {domain.addr}")
             else:
-                print(guest['name'], 'undefined', '-')
+                print(f"{guest['name']: <15} undefined  -")
 
     def _up(self, args):
+        """ create and start guests """
         for guest in self.guests:
             self.action.up(guest)
 
     def _down(self, args):
+        """ destroy and undefine guests """
         for guest in self.guests:
             self.action.down(guest)
 
     def _go(self, args):
+        """ ssh to guest """
         res = dns.resolver.Resolver()
         res.nameservers = ['192.168.122.1']
         answers = res.query(args[0] + '.')
