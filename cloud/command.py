412c674e348ea21c97bd874bcff3abffe7cdb26c
Peter McGarrigle <mcgarrigle@github.com>
2021-09-03 16:30:43 +0100

    moved project to own concerning class

--- cloud/command.py
+++ cloud/command.py
@@ -2,6 +2,8 @@ import re
 import sys
 import yaml
 import inspect
+
+from cloud.project import Project
 from cloud.guest import Guest
 from cloud.action import Action
 from cloud.hypervisor import Hypervisor
@@ -18,16 +20,6 @@ class Command:
         mapping = [ (m[0][5:], inspect.getdoc(m[1])) for m in methods ]
         return mapping
 
-    def new_guest(self, name, defn):
-        name = '-'.join(filter(None,[self.project, name]))
-        guest = Guest(name, defn)
-        domain = self.hypervisor.domain(guest.name)
-        if domain:
-            guest.state = domain.state
-            guest.addr  = domain.addr
-            guest.mac   = domain.mac
-        return guest
-
     def load_cloud_yaml(self, path):
         try:
             with open(path) as f:
@@ -35,30 +27,19 @@ class Command:
         except Exception as e:
             sys.exit(f"cannot open {path}\n{str(e)}")
 
-    def load_config(self, config):
-        self.version = config.get('version')
-        if self.version == '3':
-            pass
-        else:
-            sys.exit(f"version {self.version} not supported")
-        self.project = config.get('project')
-        self.guests  = [self.new_guest(n,g) for n,g in config['guests'].items() ]
-
-    def regex(self, glob):
-        pattern = glob.replace('*', '.*')
-        return re.compile(f"^{pattern}$")
-
-    def these(self, args):
-        if args:
-            pattern = self.regex(args[0])
-            return [ g for g in self.guests if pattern.match(g.name) ]
-        else:
-            return self.guests
+    def status(self, guest):
+        domain = self.hypervisor.domain(guest.name)
+        if domain:
+            guest.state = domain.state
+            guest.addr  = domain.addr
+            guest.mac   = domain.mac
+        return guest
 
     def _cmd_list(self, args):
         """ show status of all guests """
         up = 1
-        for guest in self.these(args):
+        for guest in self.project.these(args):
+            guest = self.status(guest)
             print(f"{guest.name: <15} {guest.state: <10} {guest.mac: <18} {guest.addr}")
             if guest.addr == '-':
                 up = 0
@@ -69,14 +50,14 @@ class Command:
     def _cmd_wait(self, args):
         """ wait for all guests to boot """
         while True:
-           booting = any([ a.addr == '-' for a in self.these(args) ])
+           booting = any([ a.addr == '-' for a in self.project.these(args) ])
            print(booting)
            if not booting: break
 
     def _cmd_inventory(self, args):
         """ create ansible inventory of all guests """
         inv = {}
-        for guest in self.guests:
+        for guest in self.project.guests:
             inv[guest.hostname] = { 'ansible_host': guest.addr }
         var = {}
         print(yaml.dump({ 'all': { 'hosts': inv, 'vars': var }}, default_flow_style=False))
@@ -91,17 +72,20 @@ class Command:
 
     def _cmd_up(self, args):
         """ create and start guests """
-        for guest in self.these(args):
+        for guest in self.project.these(args):
+            guest = self.status(guest)
             self.action.up(guest)
 
     def _cmd_stop(self, args):
         """ halt all guests """
-        for guest in self.these(args):
+        for guest in self.project.these(args):
+            guest = self.status(guest)
             self.action.stop(guest)
 
     def _cmd_down(self, args):
         """ destroy and undefine guests """
-        for guest in self.these(args):
+        for guest in self.project.these(args):
+            guest = self.status(guest)
             self.action.down(guest)
 
     def _cmd_go(self, args):
@@ -110,7 +94,7 @@ class Command:
 
     def run(self, path, cmd, args = []):
         config = self.load_cloud_yaml(path)
-        self.load_config(config)
+        self.project = Project(config)
         method = f"self._cmd_{cmd}"
         fn = eval(method)
         fn(args)
---
+++ cloud/project.py
@@ -0,0 +1,27 @@
+import re
+from cloud.guest import Guest
+
+class Project:
+
+    def guest_name(self, name):
+        return '-'.join(filter(None,[self.name, name]))
+
+    def __init__(self, config):
+        self.version = config.get('version')
+        if self.version == '3':
+            pass
+        else:
+            sys.exit(f"version {self.version} not supported")
+        self.name   = config.get('project')
+        self.guests = [ Guest(self.guest_name(n), g) for n,g in config['guests'].items() ]
+
+    def regex(self, glob):
+        pattern = glob.replace('*', '.*')
+        return re.compile(f"^{pattern}$")
+
+    def these(self, args):
+        if args:
+            pattern = self.regex(args[0])
+            return [ g for g in self.guests if pattern.match(g.name) ]
+        else:
+            return self.guests
